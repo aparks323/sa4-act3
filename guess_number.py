5e927335849827be2fd8a3c1606b0e5aba8c93bf
Alyssa Parks <aparks323@gmail.com>
2024-09-11 23:08:46 -0500

    Added number of guesses

--- guess_number.py
+++ guess_number.py
@@ -1,13 +1,15 @@
 number = 10
 print("I'm thinking of a number...")
 guess = (input("What number am I thinking of? (Press q to quit.) "))
-
+numguess = 1
 if guess == number:
     print("Congratulations! You guessed the right number.")
 elif guess != number:
         print(f"Sorry! The number was {number}.")
         guess = (input("What number am I thinking of? (Press q to quit.) "))
-
+        numguess += 1
+        print(f'You have guessed {numguess} times.')
 elif guess == 'q':
     print("Goodbye!")
+
 
